614620bda5aff848f39d5547b37977885dcecc45
jterstall <jeroen_t@hotmail.com>
2016-09-22 16:15:14 +0200

    First commit

---
+++ app/src/main/java/terstall/jeroenterstall_pset3/mainScreen.java
@@ -0,0 +1,15 @@
+package terstall.jeroenterstall_pset3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
